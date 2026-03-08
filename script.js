00f8856be35d62a968fbf35a5807a23ed133090d
Hafizur-Rahman-SD <hrrony859@gmail.com>
2026-03-09 00:51:41 +0600

    Fetch and display issues data from API

--- script.js
+++ script.js
@@ -1,2 +1,61 @@
 
+// https://phi-lab-server.vercel.app/api/v1/lab/issues
+const container = document.getElementById("issuesContainer")
+const spinner = document.getElementById("spinner")
 
+async function loadIssues(){
+
+spinner.classList.remove("hidden")
+
+const res = await fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues")
+
+const data = await res.json()
+
+displayIssues(data.data)
+
+spinner.classList.add("hidden")
+
+}
+
+loadIssues()
+
+
+
+//Display data 
+function displayIssues(issues){
+
+container.innerHTML=""
+
+issues.forEach(issue=>{
+
+const card = document.createElement("div")
+
+card.classList.add("issue-card")
+
+if(issue.status==="open"){
+card.classList.add("issue-open")
+}else{
+card.classList.add("issue-closed")
+}
+
+card.innerHTML = `
+
+<h3>${issue.title}</h3>
+
+<p>${issue.description}</p>
+
+<p>Status: ${issue.status}</p>
+<p>Author: ${issue.author}</p>
+<p>Priority: ${issue.priority}</p>
+<p>Label: ${issue.label}</p>
+
+`
+card.addEventListener("click",()=>{
+openModal(issue)
+})
+
+container.appendChild(card)
+
+})
+
+}
